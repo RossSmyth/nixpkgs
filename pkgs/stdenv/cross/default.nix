39f7d936d724b5eb9fab06c9e536eaa526661179
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-07-31 12:13:19 -0400

    stdenv: Recognize msvc when cross compiling

--- pkgs/stdenv/cross/default.nix
+++ pkgs/stdenv/cross/default.nix
@@ -78,6 +78,7 @@ lib.init bootStages
                     "musl"
                     "wasilibc"
                     "relibc"
+                    "msvc"
                   ]
                   || p.isiOS
                   || p.isGenode;
