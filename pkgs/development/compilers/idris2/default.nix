dffb129da807d5d6af739837f62bd19e9d8912c5
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-09-22 17:27:09 -0400

    idris2Packages: Make idris2Packages a proper pkgset

--- pkgs/development/compilers/idris2/default.nix
+++ pkgs/development/compilers/idris2/default.nix
@@ -1,10 +1,21 @@
-{ callPackage, idris2 }:
 {
-  inherit idris2;
-  idris2Api = callPackage ./idris2-api.nix { };
-  idris2Lsp = callPackage ./idris2-lsp.nix { };
+  lib,
+  idris2,
+  newScope,
+}:
+lib.makeScope newScope (
+  self:
+  let
+    inherit (self) callPackage;
+  in
+  {
+    inherit idris2;
+    idris2Api = callPackage ./idris2-api.nix { };
+    idris2Lsp = callPackage ./idris2-lsp.nix { };
 
-  pack = callPackage ./pack.nix { };
+    pack = callPackage ./pack.nix { };
 
-  buildIdris = callPackage ./build-idris.nix { };
-}
+    buildIdrisPackage = callPackage ./build-idris.nix { };
+    buildIdris = self.buildIdrisPackage; # Alias added 2025-09-22
+  }
+)
