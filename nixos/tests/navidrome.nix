bf767a64323c3bce11788d4b071597473915f7ae
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-07-27 11:59:42 -0400

    nixos/tests/navidrome: failing scan

--- nixos/tests/navidrome.nix
+++ nixos/tests/navidrome.nix
@@ -3,21 +3,33 @@
   name = "navidrome";
 
   nodes.machine =
-    { ... }:
+    { config, pkgs, ... }:
     {
+      # Need a user+group to own the lib dir
+      users.groups.libOwner = { };
+      users.users = {
+        libOwner = {
+          isNormalUser = true;
+          extraGroups = [ "libOwner" ];
+        };
+        # Add navidrome to the group so it can read the lib dir
+        navidrome.extraGroups = [ "libOwner" ];
+      };
+
+      systemd.tmpfiles.settings."10-lib"."/media".d = {
+        mode = "0744"; # Perms so that navidrome can read the dir
+        user = "libOwner";
+        group = "libOwner";
+      };
+
       services.navidrome = {
         enable = true;
-        plugins = with pkgs.navidromePlugins; [
-          listenbrainz-daily-playlist
-        ];
+        settings.MusicFolder = "/media";
       };
     };
-
   testScript = ''
     machine.wait_for_unit("navidrome")
-    machine.wait_for_console_text("Starting plugin manager")
-    # Make sure we saw at least one plugin load
-    machine.wait_for_console_text("plugin=listenbrainz-daily-playlist")
-    machine.wait_for_open_port(4533)
+    machine.wait_for_file("/run/navidrome/media")
+    machine.wait_for_console_text("Scanner: Finished scanning all libraries")
   '';
 }
