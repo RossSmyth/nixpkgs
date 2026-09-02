4eb403f704838b3d0b1ff4354c5a0b6d809b6c4b
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-09-02 14:34:59 -0400

    publii: drop

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2046,6 +2046,7 @@ mapAliases {
   proxmark3-rrg = throw "'proxmark3-rrg' has been renamed to/replaced by 'proxmark3'"; # Converted to throw 2025-10-27
   pscid = throw "'pscid' has been removed because it was unmaintained upstream"; # Added 2025-12-12
   psi = throw "'psi' has been removed as it depended on EOL qt5 webengine. Consider moving to psi-plus, a somewhat more maintained fork."; # Added 2026-04-15
+  publii = throw "'publii' has been removed as it was unmaintained and had an insecure Electron version."; # Added 2026-09-02
   pulp = throw "'pulp' has been removed because it was unmaintained upstream"; # Added 2025-12-12
   puppet = warnAlias "'puppet' has been replaced by 'openvox'" openvox; # Added 2026-07-07
   purescript-psa = throw "'purescript-psa' has been removed because it was unmaintained within nixpkgs"; # Added 2025-12-12
